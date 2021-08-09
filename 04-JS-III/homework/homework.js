17e48de01204de29d5063000ad73d4f073af6785
Ju <ribottojulia@gmail.com>
2021-08-08 22:50:18 -0300

    todosiguales done

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -129,7 +129,7 @@ function multiplicarArgumentos() {
   // Escribe tu código aquí:
   var multiplicationInstance = 1;
   
-  if (arguments.length >= 1) {
+  if (arguments.length > 0) {
     for (i=0;i<arguments.length;i++) {
       multiplicationInstance *= arguments[i];
     }
@@ -144,7 +144,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var cantDiezyocho = 0;
+  
+  for (i=0;i<arreglo.length;i++) {
+    if (arreglo[i] > 18) {
+      cantDiezyocho += 1;
+    }
+  }
+  return cantDiezyocho;
 }
 
 
@@ -153,7 +160,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia > 0 && (numeroDeDia === 1 || numeroDeDia === 7)) {
+    return 'Es fin de semana';
+  }
+  else {
+    return 'Es dia Laboral'
+  }
 } 
 
 
@@ -162,16 +174,37 @@ function empiezaConNueve(n) {
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
   
-}
+  if (n > 9){
+    let firstNumber = n / 10 ;
+    firstNumber = Math.trunc(firstNumber);
+    if (firstNumber === 9) {
+      return true;
+    }
+  }
+  else if (n === 9) {
+     return true;
+    }
+  return false;
+  }
 
 
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var comparador = arreglo[0];
+ 
+  if (arreglo.length > 1){ 
+    for (let i=1;i<arreglo.length;i++) {
+      if (arreglo[i] !== comparador) {   
+        return false;
+      }
+    }
+    return true;
+  }
+  return equalCheck
+}
   
-} 
-
 
 function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
